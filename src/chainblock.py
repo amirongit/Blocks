06ece735aad73b4e0d3d99fb2b2a37f73f8c20da
mr.pino <pinoq@protonmail.com>
2020-08-03 00:38:06 +0430

    improved str method for block class, changed get_hash method to be able to test without objects properties, added is_valid func to chain class

--- src/chainblock.py
+++ src/chainblock.py
@@ -7,19 +7,25 @@ class Block():
     def __init__(self, data: str, prev_block_hash: str):
         self.time_stamp = str(datetime.now())
         self.data = data
+        self.prev_block_hash = prev_block_hash
         self.hash_ = self.get_hash()
 
-    def get_hash(self):
+    def get_hash(self, prev_hash=None):
+        if not prev_hash:
+            prev_hash = self.prev_block_hash
         hasher = sha256()
-        hasher.update(bytes(self.data + self.time_stamp, 'utf-8'))
+        hasher.update(bytes(self.data + self.time_stamp + prev_hash,
+                            'utf-8'))
         return hasher.hexdigest()
 
     def is_valid(self):
         return self.get_hash() == self.hash_
 
     def __str__(self):
-        return f'Data: {self.data}, \nTimeStamp: {self.time_stamp}, \n\
-Hash: {self.hash_}\n---EndOfBlock---'
+        return f'---BlockStart---\n\
+Data: {self.data}, \nTimeStamp: {self.time_stamp}, \n\
+Hash: {self.hash_}, \nPreviousBlockHash: \
+{self.prev_block_hash}\n---BlockEnd---'
 
 
 class Chain():
@@ -32,5 +38,19 @@ class Chain():
     def add_block(self, data: str):
         block = Block(data, self.chain[-1].hash_)
         self.chain.append(block)
+
+    def is_valid(self):
+        for block in self.chain:
+            if self.chain.index(block) == 0:
+                continue
+            if block.get_hash(self.chain[
+                    self.chain.index(block) - 1].hash_) == block.get_hash():
+                pass
+            else:
+                return False
+        return True
+
+    def __str__(self):
+        return '\n\n'.join([str(block) for block in self.chain])
 
     # TODO set difficulty level.
