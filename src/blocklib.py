725dcc0d49a909765341c2e74db9501cb9ecb2f8
mr.pino <pinoq@protonmail.com>
2020-08-02 13:52:10 +0430

    added blocklib file, developing Block calsses

---
+++ src/blocklib.py
@@ -0,0 +1,19 @@
+from hashlib import sha256
+from datetime import datetime
+
+
+class Block():
+
+    def __init__(self, data: str, prev_block_hash: str):
+        self.time_stamp = str(datetime.now())
+        self.data = data
+        self.hash_ = self.get_hash()
+
+    def get_hash(self):
+        hasher = sha256()
+        hasher.update(bytes(self.data + self.time_stamp, 'utf-8'))
+        return hasher.hexdigest()
+
+    def __str__(self):
+        return f'Data: {self.data}, \nTimeStamp: {self.time_stamp}, \n\
+Hash: {self.hash_}\n---EndOfBlock---'
