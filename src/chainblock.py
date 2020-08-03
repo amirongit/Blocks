3857de3f24c1b996b37c07306991f022f9020339
mr.pino <pinoq@protonmail.com>
2020-08-04 03:11:43 +0430

    added hash difficulty to Block class

--- src/chainblock.py
+++ src/chainblock.py
@@ -1,31 +1,37 @@
-from hashlib import sha256
 from datetime import datetime
+from hashlib import sha256
+from itertools import count
 
 
 class Block():
 
-    def __init__(self, data: str, prev_block_hash: str):
+    def __init__(self, data: str, prev_block_hash: str, test_func):
         self.time_stamp = str(datetime.now())
         self.data = data
         self.prev_block_hash = prev_block_hash
-        self.hash_ = self.get_hash()
+        self.hash_, self.salt = self.get_hash(test_func)
 
-    def get_hash(self, prev_hash=None):
+    def get_hash(self, test_func, prev_hash=None):
         if not prev_hash:
             prev_hash = self.prev_block_hash
         hasher = sha256()
-        hasher.update(bytes(self.data + self.time_stamp + prev_hash,
-                            'utf-8'))
-        return hasher.hexdigest()
+        for salt in count():
+            hasher.update(bytes(self.data + self.time_stamp +
+                                prev_hash + str(salt), 'utf-8'))
+            if test_func(hasher.hexdigest()):
+                return hasher.hexdigest(), str(salt)
 
-    def is_valid(self):
-        return self.get_hash() == self.hash_
+    def is_valid(self, prev_hash, test_func):
+        hasher = sha256()
+        hasher.update(bytes(self.data + self.time_stamp +
+                            prev_hash + self.salt, 'utf-8'))
+        return self.get_hash(test_func)[0] == self.hash_
 
     def __str__(self):
         return f'---BlockStart---\n\
 Data: {self.data}, \nTimeStamp: {self.time_stamp}, \n\
 Hash: {self.hash_}, \nPreviousBlockHash: \
-{self.prev_block_hash}\n---BlockEnd---'
+{self.prev_block_hash},\nHashSalt: {self.salt}\n---BlockEnd---'
 
 
 class Chain():
