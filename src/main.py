72b784e1d42462e50338a604263c43a40fd02231
mr.pino <pinoq@protonmail.com>
2020-08-04 17:51:20 +0430

    updated main file to use new functionality

--- src/main.py
+++ src/main.py
@@ -2,20 +2,17 @@ from chainblock import Chain, Block
 
 
 def difficulty_test(hash_):
-    return hash_[:2] == 2 * '0'
+    return hash_[:1] == 1 * '0'
 
 
 my_block = Block('Genesis', '', difficulty_test)
-print(my_block, '\n')
-print('Testing the block validation: ', my_block.is_valid(difficulty_test, ''))
-print()
-my_block.data = 'Changed'
-print('Testing after data changed: ', my_block.is_valid(difficulty_test, ''),
-      '\n')
-
-my_chain = Chain(lambda hash_: hash_[:2] == 2 * '0')
-my_chain.add_block('Hi')
-print(my_chain, '\n')
-print('Testing validation of chain: ', my_chain.is_valid(), '\n')
-my_chain.chain[0].data = 'CHANGED'
-print('Testing after data changed: ', my_chain.is_valid())
+print(my_block)
+print('\nvalidating block ...', my_block.is_valid())
+my_block.data = 'CHANGED!'
+print('validating after data change ...', my_block.is_valid(), '\n')
+my_chain = Chain(lambda hash_: hash_[:2] == '0' * 2)
+my_chain.add_block('Another Block')
+print(my_chain)
+print('\nvalidating chain ...', my_chain.is_valid())
+my_chain.chain[0].data = 'CHANGED!'
+print('validating after data change ...', my_chain.is_valid())
