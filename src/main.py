e809b47d7c908ebe27d999890d4a9908dcafb1f7
mr.pino <pinoq@protonmail.com>
2020-08-08 11:32:16 +0430

    changed main file to use all the classes.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 from chainblock import Chain, Block
 
+new_block = Block('A new block!', '', lambda hash_: hash_[:2] == 2 * '0')
+print(new_block)
+print(f'validating... {new_block.is_valid()}')
+print('changing data...')
+new_block.data = 'This data is now changed!'
+print(f'validating... {new_block.is_valid()}')
 
-def difficulty_test(hash_):
-    return hash_[:1] == 1 * '0'
-
-
-my_block = Block('Genesis', '', difficulty_test)
-print(my_block)
-print('\nvalidating block ...', my_block.is_valid())
-my_block.data = 'CHANGED!'
-print('validating after data change ...', my_block.is_valid(), '\n')
-my_chain = Chain(lambda hash_: hash_[:2] == '0' * 2)
-my_chain.add_block('Another Block')
-print(my_chain)
-print('\nvalidating chain ...', my_chain.is_valid())
-my_chain.chain[0].data = 'CHANGED!'
-print('validating after data change ...', my_chain.is_valid())
+new_chain = Chain(lambda hash_: hash_[:2] == 2 * '0')
+new_chain.add_block('This is the first valid block!')
+new_chain.add_block('And this is another block.')
+print(f'validating... {new_chain.is_valid()}')
+print('changing data...')
+new_chain.chain[1].data = 'This data is now changed!'
+print(new_chain)
+print(f'validating... {new_chain.is_valid()}')
